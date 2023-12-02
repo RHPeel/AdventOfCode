e0e27fa87f3c98d994a763618c3875ba6f2c812a
RHPeel <87251403+RHPeel@users.noreply.github.com>
2023-12-02 14:47:24 -0500

    Update dcs_day2.go
    Added some comments. Refactored slightly.

--- 2023/dcs_day2.go
+++ 2023/dcs_day2.go
@@ -8,6 +8,8 @@ import (
 	//"reflect"
 )
 
+//This is going to be how we characterize individual games in our dataset.
+//Worth noting that these will eventually be the keys in a map.
 type game struct {
 	gameNumber int
 	redCount   int
@@ -16,6 +18,7 @@ type game struct {
 	power      int
 }
 
+//Generic function I use to do the error handling on strconv.Atoi
 func string_to_int(a string) int {
 	b, err := strconv.Atoi(a)
 	if err != nil {
@@ -24,6 +27,7 @@ func string_to_int(a string) int {
 	return b
 }
 
+//My standard file import.
 func fileimport(a string) []uint8 {
 
 	b, err := ioutil.ReadFile(a)
@@ -34,25 +38,36 @@ func fileimport(a string) []uint8 {
 	return b
 }
 
+//Convert the file import into an array; each line gets one entry.
 func listmaker(a []uint8) []string {
 	b := strings.Split(string(a), "\n")
 	return b
 }
 
+//This is how we convert the input into the "game" struct.
+//Our input is one of the lines from the input file.
 func process_game(a string) game {
+	//First, eliminate the colons, semicolons, and commas. Spaces are enough.
 	a = strings.Replace(a, ":", "", -1)
 	a = strings.Replace(a, ";", "", -1)
 	a = strings.Replace(a, ",", "", -1)
+	//We're going to make the line into an array; each word is its own field.
 	b := strings.Split(string(a), " ")
-	p := game{redCount: 0, blueCount: 0, greenCount: 0, power: 0}
+	//Now we'll initiate a game and store it as "p".
+	p := game{redCount: 0, blueCount: 0, greenCount: 0, power: 0, gameNumber: 0}
+	//Set the "numbercheck" variable to zero.
 	myNumberCheck := 0
+	//FOR loop that iterates through each item in the array.
 	for n := 0; n < len(b); n++ {
-		//fmt.Println(string(b[n]))
+		//The switch evaluates how we should handle each case.
 		switch b[n] {
+		//If the value is "Game" we know we can get the gameNumber in the next spot over.
 		case "Game":
 			p.gameNumber = string_to_int(b[n+1])
+		//If the value is "green," we know that the number in the previous spot is the relevant value.
 		case "green":
 			myNumberCheck = string_to_int(b[n-1])
+			//We just want to retain the highest number displayed in the record.
 			if p.greenCount < myNumberCheck {
 				p.greenCount = myNumberCheck
 			}
@@ -68,19 +83,28 @@ func process_game(a string) game {
 			}
 		}
 	}
+	//For 2B we calculate "power" as the product of the three block counts.
 	p.power = p.blueCount * p.redCount * p.greenCount
 	return p
 }
 
+//This is our process for actually taking the struct produced in process_game and making it into the map.
 func build_games(a []string) map[game]string {
+	//Create our map.
 	theGames := make(map[game]string)
+	//Iterate through every item in the array; each item is a separate line from the input.
 	for x := 0; x < len(a); x++ {
+		//process_game returns a game struct, so we'll store that in newGame.
 		newGame := process_game(a[x])
-		theGames[newGame] = "unchecked"
+		//Once we have the game completed we can determine if it is valid.
+		theGames[newGame] = determine_if_game_is_valid(newGame)
 	}
+	//We should return the map here.
 	return theGames
 }
 
+//This is pretty straightforward: we default to "valid..."
+//... and switch to invalid if any of our counts are higher than the max.
 func determine_if_game_is_valid(theGame game) string {
 	gameDecision := "valid"
 	if theGame.redCount > 12 {
@@ -95,8 +119,13 @@ func determine_if_game_is_valid(theGame game) string {
 	return gameDecision
 }
 
+//This was the 2A problem.
+//It was to just identify the "valid" games and sum up the game numbers.
+//Pretty straightforward program for this. We take the map of games as input and output an integer.
 func get_sum_of_valid_games(theGame map[game]string) int {
 	mySum := 0
+	//FOR loop syntax here: when you iterate on a map, you can designate a variable for key and value.
+	//Our key is thisGame; our value is gameDecision.
 	for thisGame, gameDecision := range theGame {
 		if gameDecision == "valid" {
 			mySum += thisGame.gameNumber
@@ -106,8 +135,13 @@ func get_sum_of_valid_games(theGame map[game]string) int {
 
 }
 
+//This was the 2B problem.
+//This is even simpler; we computed "power" during process_games.
+//So all this does is sum the powers across every object.
 func get_sum_of_powers(theGame map[game]string) int {
 	myPower := 0
+	//FOR loop syntax here: when you iterate on a map, you can designate a variable for key and value.
+	//Our key is thisGame; our value is not included at all because we don't need it.
 	for thisGame, _ := range theGame {
 		myPower += thisGame.power
 	}
@@ -119,9 +153,6 @@ func main() {
 	inputtext := fileimport("dcs_day2_input.txt")
 	instructions := listmaker(inputtext)
 	myGames := build_games(instructions)
-	for gameNumber, _ := range myGames {
-		myGames[gameNumber] = determine_if_game_is_valid(gameNumber)
-	}
 	fmt.Println("\n2A: ", get_sum_of_valid_games(myGames))
 	fmt.Println("\n2B: ", get_sum_of_powers(myGames))
 }
